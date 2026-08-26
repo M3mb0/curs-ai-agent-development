945018b1265419bf0e7738780d0b05ffd9617356
Ungureanu Cristian Dumitru <cristian.ungureanu.86@gmail.com>
2026-08-26 17:21:46 +0300

    Small change regarding a typo

--- Lectia_1/lectia1_chat.py
+++ Lectia_1/lectia1_chat.py
@@ -6,7 +6,7 @@ load_dotenv()
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 #Cream o sesiune de chat ( asta tine minte istoricul automat)
-chat = client.chats.create(model=   )
+chat = client.chats.create(model= "gemini-3.6-flash")
 
 #Primul mesaj
 response1 = chat.send_message("Ma cheama Cristian si lucrez in domeniul BPO)")
